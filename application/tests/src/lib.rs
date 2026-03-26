456baf98c7f83e35ec74567a9022e04fe03d0c5d
acriha <>
2026-03-26 23:37:59 +0200

    commit

--- application/tests/src/lib.rs
+++
@@ -1,1 +0,0 @@
-// Empty lib.rs for cargo
--- application/tests/src/main.rs
+++ application/tests/src/main.rs
@@ -1,14 +1,30 @@
-#![allow(non_snake_case)]
+use cucumber::{given, when, then, World};
+#[derive(Debug, Default, cucumber::World)]
+struct TestWorld;
 
-mod suite;
-mod archive_steps;
-use archive_steps::ArchiveWorld;
-use cucumber::World;
-
-#[tokio::main]
-async fn main() {
-    ArchiveWorld::cucumber()
-        .run_and_exit("suite/archive/archive.feature")
-        .await;
+#[given(expr = "a working system")]
+async fn a_working_system(_world: &mut TestWorld) {
+    // Setup code
 }
+
+#[when(expr = "I perform an action")]
+async fn i_perform_action(_world: &mut TestWorld) {
+    // Action code
+}
+
+#[then(expr = "I should see the expected result")]
+async fn i_should_see_expected_result(_world: &mut TestWorld) {
+    // Assertion code
+}
+
+fn main() {
+    futures::executor::block_on(async {
+        TestWorld::run("./features").await;
+    });
+}
+
+
+
+
+
 
