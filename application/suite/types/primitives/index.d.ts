67713ac45b79e6bbdaefa46e8ff0bdf4d9e866c8
acriha <supstanta_andu@yahoo.com>
2026-04-02 10:49:46 +0300

    use maven to verify JS typing in src/test/resources

---
+++ application/suite/types/primitives/index.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="conditionExpression.d.ts" />
+/// <reference path="expression.d.ts" />
+/// <reference path="maybeExpression.d.ts" />
+/// <reference path="numberExpression.d.ts" />
+/// <reference path="stringExpression.d.ts" />
+
+export {};
