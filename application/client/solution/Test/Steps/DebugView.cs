f6250ab7b6c7cd131d488aff5902bcfc4ff0eff8
acriha <>
2026-04-25 12:53:07 +0300

    commit

---
+++ application/client/solution/Test/Steps/DebugView.cs
@@ -0,0 +1,12 @@
+﻿using GdUnit4;
+
+namespace NewGameProject.Tests.XUnit;
+
+public partial class Steps
+{
+    public async Task DebugView(ISceneRunner runner)
+    {
+        runner.MaximizeView();
+        await runner.SimulateFrames(Int32.MaxValue);
+    }
+}
