b0e94ff339af0d54dc08891ba43a4e6de5edfccb
acriha <>
2026-04-27 21:41:36 +0300

    commit

--- application/runtime/src/ffi_mod/misc/free_string.rs
+++ application/runtime/src/ffi_mod/misc/free_string.rs
@@ -1,17 +1,12 @@
 use std::ffi::CString;
 use libc::c_char;
-use std::os::raw::c_void;
-
-#[link(name = "ole32")]
-extern "system" {
-    fn CoTaskMemFree(pv: *mut c_void);
-}
 
 #[export_name = "runtime_free_string"]
 pub extern "C" fn runtime_free_string(s: *mut c_char) {
     if s.is_null() { return; }
     unsafe {
-        CoTaskMemFree(s as *mut c_void);
+        // Free using the same allocator that created it (CString::into_raw)
+        let _ = CString::from_raw(s);
     }
 }
 
