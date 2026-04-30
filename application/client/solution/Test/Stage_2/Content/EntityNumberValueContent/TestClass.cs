fc73b94d9446ad0d6814917bbd4fb478a10c47f6
acriha <>
2026-04-30 22:09:16 +0300

    Add EntityNumberValueContent test case for number map

---
+++ application/client/solution/Test/Stage_2/Content/EntityNumberValueContent/TestClass.cs
@@ -0,0 +1,43 @@
+using System.Runtime.ExceptionServices;
+using CommandLine;
+using GdUnit4.Examples.Basics.Setup.Sources.UI;
+using Godot;
+using NewGameProject.Runtime;
+using NewGameProject.Tests.XUnit;
+using Vector2 = Godot.Vector2;
+
+namespace GdUnit4.Examples.Basics.Setup.Test.Stage_2.Content.EntityNumberValueContent;
+
+[TestSuite]
+public class TestClass : Steps {
+    [TestCategory("Step_2")]
+    [TestCase(Timeout = 1_000)]
+    [RequireGodotRuntime]
+    [GodotExceptionMonitor]
+    public async Task Given_panel_it_should_have_content_from_entity_number_value() {
+        AddFileToArchive("module/index.js", "index.js")
+            .AddFileToArchive("module/manifest.json", "manifest.json")
+            .AddFileToArchive("module/texture.exr", "texture.exr")
+            .EnsureDllAccessible()
+            .ProcessArchive();
+
+        var scene = LoadTestScene();
+        var rootNode = new Root();
+        var idList = RuntimeInterop.GetPanelIds();
+        foreach (var id in idList) {
+            rootNode.AddChild(new Panel(RuntimeInterop.GetPanelById(id)));
+        }
+
+        scene.AddChild(rootNode);
+        rootNode.SetSize(new Vector2() {
+            X = 1000,
+            Y = 1000
+        });
+        rootNode.SetAnchorsPreset(Control.LayoutPreset.Center);
+        await runner.SimulateFrames(1);
+
+        AssertPanelThat(rootNode.GetNode<Panel>(idList[0]))
+            .IsNonNull()
+            .HasContentText("42");
+    }
+}
