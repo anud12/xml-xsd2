0ec70b914e241185c094974b6d4c2ef7031c431e
acriha <>
2026-04-30 15:05:53 +0300

    commit

---
+++ application/client/solution/Sources/UI/ConstantTextContentNode.cs
@@ -0,0 +1,54 @@
+using Godot;
+
+public partial class ConstantTextContentNode : RichTextLabel {
+    public ConstantTextContentNode(NewGameProject.Runtime.ConstantTextContent content) {
+        Name = "content";
+        FitContent = true;
+        SetAutowrapMode(TextServer.AutowrapMode.WordSmart);
+        SetJustificationFlags(TextServer.JustificationFlag.None);
+        SetAnchorsPreset(LayoutPreset.FullRect);
+        ApplyAlignment(content.Align);
+        Text = content.Value;
+    }
+
+    private void ApplyAlignment(string align) {
+        switch (align) {
+            case "top":
+                SetHorizontalAlignment(HorizontalAlignment.Center);
+                SetVerticalAlignment(VerticalAlignment.Top);
+                break;
+            case "top-left":
+                SetHorizontalAlignment(HorizontalAlignment.Left);
+                SetVerticalAlignment(VerticalAlignment.Top);
+                break;
+            case "top-right":
+                SetHorizontalAlignment(HorizontalAlignment.Right);
+                SetVerticalAlignment(VerticalAlignment.Top);
+                break;
+            case "center":
+                SetHorizontalAlignment(HorizontalAlignment.Center);
+                SetVerticalAlignment(VerticalAlignment.Center);
+                break;
+            case "center-left":
+                SetHorizontalAlignment(HorizontalAlignment.Left);
+                SetVerticalAlignment(VerticalAlignment.Center);
+                break;
+            case "center-right":
+                SetHorizontalAlignment(HorizontalAlignment.Right);
+                SetVerticalAlignment(VerticalAlignment.Center);
+                break;
+            case "bottom":
+                SetHorizontalAlignment(HorizontalAlignment.Center);
+                SetVerticalAlignment(VerticalAlignment.Bottom);
+                break;
+            case "bottom-left":
+                SetHorizontalAlignment(HorizontalAlignment.Left);
+                SetVerticalAlignment(VerticalAlignment.Bottom);
+                break;
+            case "bottom-right":
+                SetHorizontalAlignment(HorizontalAlignment.Right);
+                SetVerticalAlignment(VerticalAlignment.Bottom);
+                break;
+        }
+    }
+}
