387ddd4aa753c525ed238479f19afdf82ee30289
acriha <supstanta_andu@yahoo.com>
2026-05-03 23:26:13 +0300

    feat: add JUnit test for stage2 panel_initialization feature

---
+++ application/suite/src/test/java/com/example/stage2/panel_initialization/PanelInitializationTest.java
@@ -0,0 +1,38 @@
+package com.example.stage2.panel_initialization;
+
+import com.example.utils.ArchiveTestBuilder;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class PanelInitializationTest {
+
+    private ArchiveTestBuilder builder;
+
+    @AfterEach
+    void tearDown() {
+        if (builder != null) builder.cleanup();
+    }
+
+    @ParameterizedTest
+    @MethodSource("panelDirectories")
+    void panelInitialization(String directory) throws Exception {
+        builder = ArchiveTestBuilder.create("features/stage2");
+
+        String csvFile = "./" + directory + "/panel.csv";
+
+        builder.runApplication()
+                .addFile("./" + directory + "/manifest.json", "./manifest.json")
+                .addFile("./" + directory + "/index.js", "./index.js")
+                .loadArchive()
+                .assertExportedPanels(csvFile);
+    }
+
+    static Stream<String> panelDirectories() {
+        return Stream.of(
+                "panel_initialization/offset"
+        );
+    }
+}
