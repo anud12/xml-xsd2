5a562d6f6b58d51f1e51a4492b638e171289778f
acriha <supstanta_andu@yahoo.com>
2026-06-07 16:04:00 +0300

    pre test

--- application/client/solution/Test/Stage_5/ElapsedTimeUnits/TestClass.cs
+++ application/client/solution/Test/Stage_5/ElapsedTimeUnits/TestClass.cs
@@ -8,7 +8,7 @@ namespace GdUnit4.Examples.Basics.Setup.Test.Stage_5.ElapsedTimeUnits;
 
 [TestSuite]
 public partial class TestClass : Steps {
-    [TestCategory("Step_3")] [TestCase] [RequireGodotRuntime]
+    [TestCategory("Step_5")] [TestCase] [RequireGodotRuntime]
     public async Task Given_RunIteration_should_increment_elapsed_time() {
         // I create a module from the first folder
         AddFileToArchive("module/index.js", "index.js")
@@ -25,7 +25,7 @@ public partial class TestClass : Steps {
         Assertions.AssertThat(RuntimeInterop.GetElapsedTimeUnits()).IsEqual(2L);
         
         
-        //Run iteration with 1 unit elapsed time
+        //Run iteration with 2 units elapsed time
         RuntimeInterop.RunIteration(2);
         Assertions.AssertThat(RuntimeInterop.GetElapsedTimeUnits()).IsEqual(4L);
     }
--- application/client/solution/Test/Stage_5/EntityTextValueUpdate/TestClass.cs
+++ application/client/solution/Test/Stage_5/EntityTextValueUpdate/TestClass.cs
@@ -8,34 +8,53 @@ namespace GdUnit4.Examples.Basics.Setup.Test.Stage_5.EntityTextValueUpdate;
 
 // [TestSuite]
 public partial class TestClass : Steps {
-    [TestCategory("Step_3")]
+    [TestCategory("Step_5")]
     [TestCase]
     [RequireGodotRuntime]
     public async Task Given_panel_it_should_emit_action_on_clic() {
         // I create a module from the first folder
-        // AddFileToArchive("module/index.js", "index.js")
-        //     .AddFileToArchive("module/manifest.json", "manifest.json")
-        //     .AddFileToArchive("module/texture.exr", "texture.exr")
-        //     .EnsureDllAccessible()
-        //     .ProcessArchive();
-        //
-        // var scene = LoadTestScene();
-        // var rootNode = new RootNode();
-        // var idList = RuntimeInterop.GetPanelIds();
-        //
-        // scene.AddChild(rootNode);
-        // rootNode.SetSize(new Vector2() {
-        //     X = 1000,
-        //     Y = 1000
-        // });
-        // rootNode.SetAnchorsPreset(Control.LayoutPreset.Center);
-        //         await runner.SimulateFrames(1);
-        //
-        // // var assertions = AssertPanelThat(rootNode.GetNode<Panel>(idList[0]))
-        // //     .HasContentText("key");
-        //
-        // await runner.SimulateFrames(1);
-        // // RuntimeInterop.SetEntityTextMapValue("entity_id", "key", "newTextValue");
-        // // assertions.HasContentText("textValue");
+        AddFileToArchive("module/index.js", "index.js")
+            .AddFileToArchive("module/manifest.json", "manifest.json")
+            .AddFileToArchive("module/texture.exr", "texture.exr")
+            .EnsureDllAccessible()
+            .ProcessArchive();
+        
+                
+        var scene = LoadTestScene();
+        var rootNode = new RootNode();
+        var idList = RuntimeInterop.GetPanelIds();
+
+        scene.AddChild(rootNode);
+        rootNode.SetSize(new Vector2() {
+            X = 1000,
+            Y = 1000
+        });
+        rootNode.SetAnchorsPreset(Control.LayoutPreset.Center);
+        await runner.SimulateFrames(1);
+
+        var assertions = AssertPanelThat(rootNode.GetNode<Panel>(idList[0]))
+            .HasContentText("0");
+
+        
+        //Run iteration with 1 unit elapsed time
+        RuntimeInterop.RunIteration(1);
+        await runner.SimulateFrames(1);
+        Assertions.AssertThat(RuntimeInterop.GetElapsedTimeUnits()).IsEqual(1L);
+        assertions.HasContentText("1");
+        
+        
+        //Run iteration with 1 unit elapsed time
+        RuntimeInterop.RunIteration(1);
+        await runner.SimulateFrames(1);
+        Assertions.AssertThat(RuntimeInterop.GetElapsedTimeUnits()).IsEqual(2L);
+        assertions.HasContentText("2");
+        
+        
+        //Run iteration with 2 units elapsed time
+        RuntimeInterop.RunIteration(2);
+        await runner.SimulateFrames(1);
+        Assertions.AssertThat(RuntimeInterop.GetElapsedTimeUnits()).IsEqual(4L);
+        
+        assertions.HasContentText("4");
     }
 }
