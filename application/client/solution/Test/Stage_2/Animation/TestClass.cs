ff1ee8031cd123fca360e0e0b6b0f367616f9620
acriha <>
2026-07-31 14:14:20 +0300

    add assert screenshots

--- application/client/solution/Test/Stage_2/Animation/TestClass.cs
+++ application/client/solution/Test/Stage_2/Animation/TestClass.cs
@@ -39,61 +39,73 @@ public partial class TestClass : Steps {
 
             AssertPanelThat(fastPanel).IsNonNull();
             AssertPanelThat(slowPanel).IsNonNull();
-
+            AssertScreenshot("iteration_0.png");
+            
             RuntimeInterop.RunIteration(1);
             await runner.SimulateFrames(1);
             AssertPanelThat(fastPanel).HasBackgroundTexture("frame_1.png");
             AssertPanelThat(slowPanel).HasBackgroundTexture("frame_1.png");
+            AssertScreenshot("iteration_1.png");
 
             RuntimeInterop.RunIteration(1);
             await runner.SimulateFrames(1);
             AssertPanelThat(fastPanel).HasBackgroundTexture("frame_2.png");
             AssertPanelThat(slowPanel).HasBackgroundTexture("frame_1.png");
+            AssertScreenshot("iteration_2.png");
 
             RuntimeInterop.RunIteration(1);
             await runner.SimulateFrames(1);
             AssertPanelThat(fastPanel).HasBackgroundTexture("frame_3.png");
             AssertPanelThat(slowPanel).HasBackgroundTexture("frame_2.png");
+            AssertScreenshot("iteration_3.png");
 
             RuntimeInterop.RunIteration(1);
             await runner.SimulateFrames(1);
             AssertPanelThat(fastPanel).HasBackgroundTexture("frame_4.png");
             AssertPanelThat(slowPanel).HasBackgroundTexture("frame_2.png");
+            AssertScreenshot("iteration_4.png");
+            
+            RuntimeInterop.RunIteration(1);
+            await runner.SimulateFrames(1);
+            AssertPanelThat(fastPanel).HasBackgroundTexture("frame_5.png");
+            AssertPanelThat(slowPanel).HasBackgroundTexture("frame_3.png");
+            AssertScreenshot("iteration_5.png");
 
             RuntimeInterop.RunIteration(1);
             await runner.SimulateFrames(1);
             AssertPanelThat(fastPanel).HasBackgroundTexture("frame_5.png");
             AssertPanelThat(slowPanel).HasBackgroundTexture("frame_3.png");
-
-            RuntimeInterop.RunIteration(1);
-            await runner.SimulateFrames(1);
-            AssertPanelThat(fastPanel).HasBackgroundTexture("frame_5.png");
-            AssertPanelThat(slowPanel).HasBackgroundTexture("frame_3.png");
-
-            RuntimeInterop.RunIteration(1);
-            await runner.SimulateFrames(1);
-            AssertPanelThat(fastPanel).HasBackgroundTexture("frame_5.png");
-            AssertPanelThat(slowPanel).HasBackgroundTexture("frame_4.png");
+            AssertScreenshot("iteration_6.png");
 
             RuntimeInterop.RunIteration(1);
             await runner.SimulateFrames(1);
             AssertPanelThat(fastPanel).HasBackgroundTexture("frame_5.png");
             AssertPanelThat(slowPanel).HasBackgroundTexture("frame_4.png");
+            AssertScreenshot("iteration_7.png");
+
+            RuntimeInterop.RunIteration(1);
+            await runner.SimulateFrames(1);
+            AssertPanelThat(fastPanel).HasBackgroundTexture("frame_5.png");
+            AssertPanelThat(slowPanel).HasBackgroundTexture("frame_4.png");
+            AssertScreenshot("iteration_8.png");
 
             RuntimeInterop.RunIteration(1);
             await runner.SimulateFrames(1);
             AssertPanelThat(fastPanel).HasBackgroundTexture("frame_5.png");
             AssertPanelThat(slowPanel).HasBackgroundTexture("frame_5.png");
+            AssertScreenshot("iteration_9.png");
 
             RuntimeInterop.RunIteration(1);
             await runner.SimulateFrames(1);
             AssertPanelThat(fastPanel).HasBackgroundTexture("frame_5.png");
             AssertPanelThat(slowPanel).HasBackgroundTexture("frame_5.png");
+            AssertScreenshot("iteration_10.png");
 
             RuntimeInterop.RunIteration(1);
             await runner.SimulateFrames(1);
             AssertPanelThat(fastPanel).HasBackgroundTexture("frame_5.png");
             AssertPanelThat(slowPanel).HasBackgroundTexture("frame_5.png");
+            AssertScreenshot("iteration_11.png");
         }
         catch (Exception e) {
             Assertions.AssertThat(true)
